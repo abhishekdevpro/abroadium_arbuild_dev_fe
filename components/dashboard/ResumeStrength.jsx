8b8ebd3237ff6928d6c683876955930e4102e417
Anita Chouhan <fe.dev2.ba2024@gmail.com>
2025-03-27 17:45:05 +0530

    hide btn improveats

--- components/dashboard/ResumeStrength.jsx
+++ components/dashboard/ResumeStrength.jsx
@@ -370,11 +370,16 @@ const ResumeStrength = ({ score, strength, resumeId }) => {
                 Improve Resume
               </button>
               <button
-                disabled={strength.ats_score === 10}
+                disabled={strength.ats_score === 10 || !resumeId}
                 onClick={() => setIsModalOpen(true)}
+                // className={`px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition-colors ${
+                //   !strength.ats_score === 10 || !resumeId
+                // }?"opacity-50 cursor-not-allowed" :""}`}
                 className={`px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition-colors ${
-                  !strength.ats_score === 10
-                }?"opacity-50 cursor-not-allowed" :""}`}
+                  strength.ats_score === 10 || !resumeId
+                    ? "opacity-50 cursor-not-allowed"
+                    : ""
+                }`}
               >
                 Improve ATS
               </button>
