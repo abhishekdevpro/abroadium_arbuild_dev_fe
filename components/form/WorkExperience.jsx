0de79ab048c4a6248942325ab17866462c38242b
Rishi kashyap <kashyap1strishi@gmail.com>
2025-04-28 20:22:26 +0530

    country code added

--- components/form/WorkExperience.jsx
+++ components/form/WorkExperience.jsx
@@ -668,35 +668,12 @@ const WorkExperience = () => {
   //   );
   // };
   const removeWork = (index) => {
-    // Check if this is the last work experience entry
     if ((resumeData.workExperience || []).length <= 1) {
       toast.warn("At least one work experience entry is required");
-      // setValidationErrors({
-      //   ...validationErrors,
-      //   general: "At least one work experience entry is required"
-      // });
-
-      // // Clear the error message after 3 seconds
-      // setTimeout(() => {
-      //   const updatedErrors = {...validationErrors};
-      //   delete updatedErrors.general;
-      //   setValidationErrors(updatedErrors);
-      // }, 3000);
-      return; // Don't remove if it's the last one
+      return; 
     }
-
     const newworkExperience = [...(resumeData.workExperience || [])];
     newworkExperience.splice(index, 1);
-
-    // Clear any errors related to this index
-    // const updatedErrors = {};
-    // Object.keys(validationErrors).forEach(key => {
-    //   if (!key.startsWith(`${index}-`)) {
-    //     updatedErrors[key] = validationErrors[key];
-    //   }
-    // });
-    // setValidationErrors(updatedErrors);
-
     setResumeData({ ...resumeData, workExperience: newworkExperience });
     setExpandedExperiences(
       expandedExperiences
